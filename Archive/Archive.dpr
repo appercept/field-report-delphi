5ea28a24cc0d3238ffca3ca5c4b881461e8468b3
Richard Hatherall <richard@appercept.com>
2021-12-05 16:21:50 +0000

    Add Archive command line tool

---
+++ Archive/Archive.dpr
@@ -0,0 +1,90 @@
+﻿program Archive;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  AWS.Core,
+  AWS.S3,
+  AWS.SQS,
+  Settings in 'Settings.pas',
+  System.SysUtils,
+  System.JSON;
+
+var
+  Options: IAWSOptions;
+  SQSOptions: ISQSOptions;
+  SQSClient: ISQSClient;
+  QueuePoller: ISQSQueuePoller;
+
+const
+  LOG_DATE_FORMAT = 'yyyymmdd''T''hhnnss';
+
+procedure Log(const AMessage: string; ANewLine: Boolean = True);
+begin
+  if ANewLine then
+    Writeln(Format('[%s] %s', [FormatDateTime(LOG_DATE_FORMAT, Now), AMessage]))
+  else
+    Write(Format('[%s] %s', [FormatDateTime(LOG_DATE_FORMAT, Now), AMessage]));
+end;
+
+procedure ArchiveReport(const AReportKey: string);
+var
+  LS3Options: IS3Options;
+  LS3Client: IS3Client;
+  LRequest: IS3CopyObjectRequest;
+  LResponse: IS3CopyObjectResponse;
+begin
+  Log(Format('Archiving report ''%s''...', [AReportKey]), False);
+  LS3Options := TS3Options.Create(Options);
+  LS3Client := TS3Client.Create(LS3Options);
+  LRequest := TS3CopyObjectRequest.Create(
+    ArchiveReportsBucket,
+    Format('s3://%s/%s', [InboundReportsBucket, AReportKey]),
+    AReportKey
+  );
+  LResponse := LS3Client.CopyObject(LRequest);
+  if LResponse.IsSuccessful then
+    Log('done.')
+  else
+    Log('error.');
+end;
+
+procedure ProcessArchiveRequest(const AMessage: ISQSMessage);
+var
+  LJSON: TJSONObject;
+  LReportKey: string;
+begin
+  LJSON := TJSONObject.ParseJSONValue(AMessage.Body) as TJSONObject;
+  if LJSON.TryGetValue<string>('', LReportKey) then
+    ArchiveReport(LReportKey);
+end;
+
+begin
+  try
+    // Initialize common options.
+    Options := TAWSOptions.Create;
+    Options.AccessKeyId := AccessKeyId;
+    Options.SecretAccessKey := SecretAccessKey;
+    Options.Region := AwsRegion;
+
+    SQSOptions := TSQSOptions.Create(Options);
+    SQSClient := TSQSClient.Create(SQSOptions);
+    QueuePoller := TSQSQueuePoller.Create(ArchiveQueue, SQSClient);
+
+    Log('Waiting for archive report requests...');
+    QueuePoller.Poll(
+      procedure(const AMessages: TSQSMessages)
+      var
+        LMessage: ISQSMessage;
+      begin
+        for LMessage in AMessages do
+          ProcessArchiveRequest(LMessage);
+      end
+    );
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
---
+++ Archive/Settings.pas
@@ -0,0 +1,15 @@
+﻿unit Settings;
+
+interface
+
+const
+  AccessKeyId = '';
+  SecretAccessKey = '';
+  AwsRegion = '';
+  ArchiveQueue = '';
+  ArchiveReportsBucket = '';
+  InboundReportsBucket = '';
+
+implementation
+
+end.
